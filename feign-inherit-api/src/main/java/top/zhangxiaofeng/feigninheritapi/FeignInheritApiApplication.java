decc505bfa1d0ea3d7b24f969a189c578e0a3ce1
lionng <154704230@qq.com>
2021-04-05 21:39:18 +0800

    0405

--- feign-inherit-api/src/main/java/top/zhangxiaofeng/feigninheritapi/FeignInheritApiApplication.java
+++ feign-inherit-api/src/main/java/top/zhangxiaofeng/feigninheritapi/FeignInheritApiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients(basePackages = "top.zhangxiaofeng.feigninheritapi.client")
+//@EnableFeignClients(basePackages = "top.zhangxiaofeng.feigninheritapi.client")
 public class FeignInheritApiApplication {
 
     public static void main(String[] args) {
